6e51a876000c938d8d69790c9d029553cd616495
Zarubeydor <tratatoon@gmail.com>
2016-07-16 12:56:27 +0400

    Add files via upload

--- FizzBuzz/main.cpp
+++ FizzBuzz/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    for( int i = 0; i <= 100; i++ )
+    {
+        if( a % 3 == 0)
+        {
+            cout << "Fizz" << endl;
+        } else if( a % 5 == 0 )
+        {
+            cout << "Buzz" << endl;
+        } else if( a % 3 && a % 5 == 0 )
+        {
+            cout << "FizzBuzz" << endl;
+        } else {
+            cout << a << endl;
+        }
+    a += 1;
+    }
+    cout << "Enjoy!";
+    return 0;
+}
